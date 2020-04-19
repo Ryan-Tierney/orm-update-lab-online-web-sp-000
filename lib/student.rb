73e1c7f30883776827e7dc14bd022adc349ed77c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 16:20:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,8 +68,4 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.name)
   end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
 end
