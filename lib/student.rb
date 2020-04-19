d46313dd0c910ff18b3fc53213dabaad6214ed63
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 16:31:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
   
   def update 
     sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.name)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
