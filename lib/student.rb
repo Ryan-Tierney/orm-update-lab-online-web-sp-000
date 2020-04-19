349c0a151949a8a24fd98218c9c88b47deeb4cc5
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 16:53:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    row = DB[:conn].execute(sql, name)[0]
+    row = DB[:conn].execute(sql, name).map |row|
     self.new_from_db(row)
   end 
   
