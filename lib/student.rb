1c014b11613c8133cb98a01cf9bbd8a617285df3
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 16:09:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
-    son
+    student
   end
 
   # Remember, you can access your database connection anywhere in this class
