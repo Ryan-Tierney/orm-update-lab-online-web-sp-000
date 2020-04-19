bcd520a4f91799dc41c9fa3216f97e093d6745ce
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 16:13:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,14 @@ class Student
     student
   end
   
-  def self.new_from_db 
+  def self.new_from_db(array)
+  end 
+  
+  def self.find_by_name(name)
+  end 
+  
+  def update 
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
